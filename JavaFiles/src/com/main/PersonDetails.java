d0d722447a2847a2745231bc572b0c59daccb9d7
KalaiVani V <kalai-4595@kalai-4595.csez.zohocorpin.com>
2020-05-21 22:10:19 +0530

    Dynamic input from the user

--- JavaFiles/src/com/main/PersonDetails.java
+++ JavaFiles/src/com/main/PersonDetails.java
@@ -1,45 +1,29 @@
-package com.main;
+ackage com.main;
 import com.main.pojo.*;
+import java.util.Scanner;
 
 public class PersonDetails
 {
 	public static void main(String[] args)
-	{
-		Person jega = new Person("Jegathees",23,"Male");
-		Person kalai = new Person("Kalaivani",26,"Female");
-
-		jega.printPerson();
-		kalai.printPerson();
-
+	
+	{	String name;
+		int age;
+		String gender;
+		Scanner sc= new Scanner(System.in); 
+		// Person jega = new Person("Jegathees",23,"Male");
+		// Person kalai = new Person("Kalaivani",26,"Female");
+		// jega.printPerson()
+		// kalai.printPerson();
 		//Person.print();
+
+		System.out.println("Enter the Person details:");
+		System.out.print("Enter name: ");
+		name= sc.nextLine();
+		System.out.print("Enter gender(Male/Female): ");
+		gender=sc.next();
+		System.out.print("Enter age: ");
+		age= sc.nextInt();
+		Person unknown = new Person(name,age,gender);
+		unknown.printPerson();
 	}
 }
-
-
-/*
-TAsk 1:
-Enter the persion 1 details:
-Name:
-Jegathees
-Age:
-23
-Task 2:
-Enter the no of persons
-2
-Enter the persion 1 details:
-Name:
-Jegathees
-Age:
-23
-Gender:
-Male
-Enter the persion 2 details:
-Name:
-Kalai
-Age:
-26
-Gender:
-Female
-Jegathess 23 Male
-kalu 26 Female
- */
